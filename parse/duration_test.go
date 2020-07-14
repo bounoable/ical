33b015110bd102954b60ee34b5f8c2d8ee50099e
Saman Hosseini <bounoable@gmail.com>
2020-07-14 19:49:53 +0200

    refactor(parse): simplify test

--- parse/duration_test.go
+++ parse/duration_test.go
@@ -8,57 +8,24 @@ import (
 )
 
 func TestParseDuration(t *testing.T) {
-	tests := []struct {
-		raw      string
-		expected time.Duration
-	}{
-		{
-			raw:      "PT10S",
-			expected: 10 * time.Second,
-		},
-		{
-			raw:      "PT100S",
-			expected: 100 * time.Second,
-		},
-		{
-			raw:      "PT8M",
-			expected: 8 * time.Minute,
-		},
-		{
-			raw:      "PT5M40S",
-			expected: 5*time.Minute + 40*time.Second,
-		},
-		{
-			raw:      "PT4H",
-			expected: 4 * time.Hour,
-		},
-		{
-			raw:      "PT8H2M",
-			expected: 8*time.Hour + 2*time.Minute,
-		},
-		{
-			raw:      "PT2H10M2S",
-			expected: 2*time.Hour + 10*time.Minute + 2*time.Second,
-		},
-		{
-			raw:      "P4W",
-			expected: 4 * 7 * 24 * time.Hour,
-		},
-		{
-			raw:      "P7D",
-			expected: 7 * 24 * time.Hour,
-		},
-		{
-			raw:      "P7DT4H10S",
-			expected: 7*24*time.Hour + 4*time.Hour + 10*time.Second,
-		},
+	tests := map[string]time.Duration{
+		"PT10S":     10 * time.Second,
+		"PT100S":    100 * time.Second,
+		"PT8M":      8 * time.Minute,
+		"PT5M40S":   5*time.Minute + 40*time.Second,
+		"PT4H":      4 * time.Hour,
+		"PT8H2M":    8*time.Hour + 2*time.Minute,
+		"PT2H10M2S": 2*time.Hour + 10*time.Minute + 2*time.Second,
+		"P4W":       4 * 7 * 24 * time.Hour,
+		"P7D":       7 * 24 * time.Hour,
+		"P7DT4H10S": 7*24*time.Hour + 4*time.Hour + 10*time.Second,
 	}
 
-	for _, test := range tests {
-		t.Run(test.raw, func(t *testing.T) {
-			t.Run("no sign", testParseDuration(test.raw, test.expected))
-			t.Run("plus sign", testParseDuration("+"+test.raw, +test.expected))
-			t.Run("minus sign", testParseDuration("-"+test.raw, -test.expected))
+	for raw, expected := range tests {
+		t.Run(raw, func(t *testing.T) {
+			t.Run("no sign", testParseDuration(raw, expected))
+			t.Run("plus sign", testParseDuration("+"+raw, +expected))
+			t.Run("minus sign", testParseDuration("-"+raw, -expected))
 		})
 	}
 }
